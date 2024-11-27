9e9ed9aa1d1836c916d924f9ababdf03725fd558
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2024-11-27 08:08:30 -0500

    Created the chat bubble, with customized styling as per the role

--- app/components/Bubble.tsx
+++ app/components/Bubble.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
-type Props = {}
-
 const Bubble = ({ message }) => {
+
+    const { content, role } = message;
+
     return (
-        <div className="bubble"></div>
+        <div className={`${role}-bubble`}>{content}</div>
     )
 }
 
